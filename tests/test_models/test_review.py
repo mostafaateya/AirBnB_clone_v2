8190b4c71a271deba3bff2f213c223d5ed4b7707
AHMEDKISHK7 <a.kishk9437@gmail.com>
2023-09-20 14:11:19 -0700

    AHMEDKISHK

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
-from models.review import Review
+import os
+
+from models.city import City
+from tests.test_models.test_base_model import TestBasemodel
 
 
-class test_review(test_basemodel):
-    """ """
+class TestState(TestBasemodel):
+    """Test model for state"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+        self.args = args
+        self.kwargs = kwargs
 
-    def test_place_id(self):
-        """ """
+    def testPlaceId(self):
+        """Test [place id]"""
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertEqual(
+            type(new.place_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
-    def test_user_id(self):
-        """ """
+    def TestUserID(self):
+        """Test user """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertEqual(
+            type(new.user_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
-    def test_text(self):
-        """ """
+    def TestText(self):
+        """Test text"""
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertEqual(
+            type(new.text),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
-from models.state import State
+import os
+
+from models.city import City
+from tests.test_models.test_base_model import TestBasemodel
 
 
-class test_state(test_basemodel):
-    """ """
+class TestState(TestBasemodel):
+    """Test model for state"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+        self.args = args
+        self.kwargs = kwargs
 
-    def test_name3(self):
-        """ """
+    def TestName(self):
+        """Testing the name"""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
